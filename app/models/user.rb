ce3df714658619b7db4d287998e85258f88398ac
JackoDev <untaljacko@gmail.com>
2021-03-03 19:00:31 -0500

    [new] integrating devise gem, modify sing up view ann application controller for permit username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,33 @@
 class User < ApplicationRecord
-    has_many :socials, inverse_of: :user
-    has_one_attached :user_image
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :socials, inverse_of: :user
+  has_one_attached :user_image
 
-    validates :user_image, content_type: { with: ["image/png", "image/jpg"], message: "Only .jpg and .png files are allowed!" }
-    validates :username, presence: true,
-                        uniqueness: { case_sensitive: false },
-                        length: { minimum: 3, maximum: 25 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true,
-                    uniqueness: { case_sensitive: false },
-                    length: { maximum: 105 },
-                    format: { with: VALID_EMAIL_REGEX }
+  validates :user_image, content_type: { with: ["image/png", "image/jpg"], message: "Only .jpg and .png files are allowed!" }
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                  uniqueness: { case_sensitive: false },
+                  length: { maximum: 105 },
+                  format: { with: VALID_EMAIL_REGEX }
 
-    accepts_nested_attributes_for :socials, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :socials, allow_destroy: true, reject_if: :all_blank
 
-    def thumbnail
-        if user_image.attached?
-            user_image.variant(resize: '100x100')
-        end
+  def thumbnail
+    if user_image.attached?
+      user_image.variant(resize: '100x100')
     end
+  end
 
-    def profile_img
-        if user_image.attached?
-            user_image.variant(resize: '400x400')
-        end
+  def profile_img
+    if user_image.attached?
+      user_image.variant(resize: '400x400')
     end
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_231419) do
+ActiveRecord::Schema.define(version: 2021_03_03_151957) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,12 @@ ActiveRecord::Schema.define(version: 2021_02_25_231419) do
     t.string "first_name"
     t.string "last_name"
     t.text "bio"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
